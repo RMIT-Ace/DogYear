f3c5e61714fd9395aa4adc830a66e906d60ce079
Ace <PrimeCoder.Authors@gmail.com>
2025-02-19 14:12:53 +1100

    Initial Commit

---
+++ DogYear/DogYearApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogYearApp.swift
+//  DogYear
+//
+//  Created by ace on 19/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct DogYearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
